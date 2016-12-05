bcb826210d6830f514d38d3eed7fee22d4182935
Max? <mgrinchenko@forcemktg.com>
2016-12-05 16:57:48 -0500

    created login page

--- cp.py
+++ cp.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route('/login')
+def login():
+    return render_template('login.pug')
 
 
 if __name__ == '__main__':
