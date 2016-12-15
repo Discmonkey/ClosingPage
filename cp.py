58f32b388095455958797c9cedafa3f1858924b8
Max? <mgrinchenko@forcemktg.com>
2016-12-15 14:13:33 -0500

    created endpoints for logging in and registering users

--- cp.py
+++ cp.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
+
 
 @app.route('/')
 def index():
     return render_template('index.pug')
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.pug')
+    if request.method == 'GET':
+        return render_template('login.pug')
+    elif request.method == 'POST':
+        user_name = request.form['user_name']
+        password = request.form['password']
+
+@app.route('/register', methods=['POST'])
+def register():
+    user_name = request.form['user_name']
+    password = request.form['password']
+
+
 
 @app.route('/linkedInAuth')
 def linked_in_auth():
