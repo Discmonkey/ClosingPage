14ef574bca3007ef96d4961ed9f3c1127e60d61b
maxg <frismo98@gmail.com>
2016-12-24 20:42:04 -0600

    production build

--- cp.py
+++ cp.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect
-from flask_login import LoginManager, login_required, current_user, login_user, logout_user, session
+from flask import Flask, render_template, request, redirect, session
+from flask_login import LoginManager, login_required, current_user, login_user, logout_user
 from apis.PostgresConnection import PostGres
 from apis.LinkedIn import LinkedIn
 import requests
